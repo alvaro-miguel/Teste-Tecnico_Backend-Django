361895b0772879e226981587e2f34981ab029197
alvaro-miguel <alvaromiguelrodrigues12@gmail.com>
2026-08-23 20:25:35 -0300

    feat: implementando serializers ao sistema

---
+++ agendamentos/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+from .models import Usuario, Especialidade, Especialista, Paciente, Agenda, HorarioGerado, Consulta
+
+class EspecialidadeSerializer(serializers.ModelSerializer):
+    class Meta:     
+        model = Especialidade
+        fields = ['id', 'nome_especialidade', 'ativo']
+
+
+class EspecialistaSerializer(serializers.ModelSerializer):
+    especialidade_detalhe = EspecialidadeSerializer(source='especialidade', read_only=True)
+    nome = serializers.CharField(source='usuario.first_name', read_only=True)
+    
+    class Meta:
+        model = Especialista
+        fields = ['id', 'nome', 'crm', 'especialidade', 'especialidade_detalhe', 'ativo']
+
+
+class PacienteSerializer(serializers.ModelSerializer):
+    nome = serializers.CharField(source='usuario.first_name', read_only=True)
+    telefone = serializers.CharField(source='usuario.telefone', read_only=True)
+    
+    class Meta:
+        model = Paciente
+        fields = ['id', 'nome', 'telefone', 'ativo']
+
+
+class HorarioGeradoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HorarioGerado
+        fields = ['id', 'horario_inicio', 'horario_fim', 'status']
+
+
+class AgendaSerializer(serializers.ModelSerializer):
+    horarios = HorarioGeradoSerializer(many=True, read_only=True)
+    nome_especialista = serializers.CharField(source='especialista.usuario.first_name', read_only=True)
+
+    class Meta:
+        model = Agenda
+        fields = [
+            'id', 'especialista', 'nome_especialista', 'dias_semana', 
+            'hora_inicio_expediente', 'hora_fim_expediente', 
+            'quantidade_vagas_dia', 'horarios', 'ativo'
+        ]
+
+
+class ConsultaSerializer(serializers.ModelSerializer):
+    nome_paciente = serializers.CharField(source='paciente.usuario.first_name', read_only=True)
+    data_hora = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Consulta
+        fields = ['id', 'paciente', 'nome_paciente', 'horario_gerado', 'data_hora', 'ativo']
+
+    def get_data_hora(self, obj):
+        return f"{obj.horario_gerado.agenda.get_dias_semana_display()} às {obj.horario_gerado.horario_inicio}"

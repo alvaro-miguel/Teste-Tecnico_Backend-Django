e832db226af17b7d0afb11297a780869c3f8f0a2
alvaro-miguel <alvaromiguelrodrigues12@gmail.com>
2026-08-25 20:57:26 -0300

    feat: Implementando rotas de autenticacao JWT

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/agendamentos/', include('agendamentos.urls')),
     path('api/usuarios/', include('usuarios.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

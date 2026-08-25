ea6e06bdb28889173d18c0b4b3adcdf5b73b397d
alvaro-miguel <alvaromiguelrodrigues12@gmail.com>
2026-08-24 22:17:16 -0300

    refactor: renomeando CommonModel e detalhando a modelagem

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -10,7 +10,7 @@ class ActiveManager(models.Manager):
         return super().get_queryset().filter(ativo=True)
 
 
-class BaseModel(models.Model):
+class CommonModel(models.Model):
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
     ativo = models.BooleanField(default=True)
@@ -25,14 +25,35 @@ class BaseModel(models.Model):
         self.save()
 
 
-class Usuario(AbstractUser):
-    TIPO_USUARIO_CHOICES = (
-        ('ESPECIALISTA','Especialista'),
-        ('PACIENTE', 'Paciente'),
-        ('INTERNO', 'Equipe interna')
-    )
+class TipoUsuario(models.TextChoices):
+    ESPECIALISTA = 'ESPECIALISTA', 'Especialista'
+    PACIENTE = 'PACIENTE', 'Paciente'
+    INTERNO = 'INTERNO', 'Interno'
 
-    tipo_usuario = models.CharField(max_length=15, choices=TIPO_USUARIO_CHOICES, null=True, blank=True)
+
+class DiasDaSemana(models.IntegerChoices):
+    SEGUNDA = 0, 'Segunda-feira'
+    TERCA = 1, 'Terça-feira'
+    QUARTA = 2, 'Quarta-feira'
+    QUINTA = 3, 'Quinta-feira'
+    SEXTA = 4, 'Sexta-feira'
+    SABADO = 5, 'Sábado'
+    DOMINGO = 6, 'Domingo'
+
+
+class StatusHorario(models.TextChoices):
+    DISPONIVEL = 'DISPONIVEL', 'Disponível'
+    RESERVADO = 'RESERVADO', 'Reservado'
+
+
+class Usuario(AbstractUser):
+
+    tipo_usuario = models.CharField(
+        max_length=15,
+        choices=TipoUsuario.choices,
+        null=True,
+        blank=True
+    )
     cpf = models.CharField(max_length=14, unique=True, null=True, blank=True)
     telefone = models.CharField(max_length=20, null=True, blank=True)
 
@@ -40,14 +61,14 @@ class Usuario(AbstractUser):
         return f"{self.first_name} - {self.tipo_usuario}"
 
 
-class Especialidade(BaseModel):
+class Especialidade(CommonModel):
     nome_especialidade = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.nome_especialidade
 
 
-class Especialista(BaseModel):
+class Especialista(CommonModel):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='especialista_perfil')
     especialidade = models.ForeignKey(Especialidade, on_delete=models.PROTECT, related_name='especialistas')
     crm = models.CharField(max_length=20, unique=True)
@@ -56,26 +77,19 @@ class Especialista(BaseModel):
         return f"Dr(a). {self.usuario.first_name} - CRM: {self.crm}"
 
 
-class Paciente(BaseModel):
+class Paciente(CommonModel):
     usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name='paciente_perfil')
 
     def __str__(self):
         return self.usuario.first_name or self.usuario.username
 
 
-class Agenda(BaseModel):
-    DIAS_SEMANA_CHOICE = (
-        (0, 'Segunda-feira'),
-        (1, 'Terça-feira'),
-        (2, 'Quarta-feira'),
-        (3, 'Quinta-feira'),
-        (4, 'Sexta-feira'),
-        (5, 'Sábado'),
-        (6, 'Domingo'),
-    )
+class Agenda(CommonModel):
 
     especialista = models.ForeignKey(Especialista, on_delete=models.CASCADE, related_name='agendas')
-    dias_semana = models.IntegerField(choices=DIAS_SEMANA_CHOICE)
+    dias_semana = models.IntegerField(
+        choices=DiasDaSemana.choices
+    )
     hora_inicio_expediente = models.TimeField()
     hora_fim_expediente = models.TimeField()
     quantidade_vagas_dia = models.IntegerField(validators=[MinValueValidator(1)])
@@ -90,23 +104,23 @@ class Agenda(BaseModel):
         return f"Agenda: {self.especialista} - {self.get_dias_semana_display()}"
 
 
-class HorarioGerado(BaseModel):
-    STATUS_CHOICE = (
-        ('DISPONIVEL', 'Disponível'),
-        ('RESERVADO', 'Reservado')
-    )
+class HorarioGerado(CommonModel):
 
     agenda = models.ForeignKey(Agenda, on_delete=models.CASCADE, related_name='horarios')
     data = models.DateField()
     horario_inicio = models.TimeField()
     horario_fim = models.TimeField()
-    status = models.CharField(max_length=15, choices=STATUS_CHOICE, default='DISPONIVEL')
+    status = models.CharField(
+        max_length=15,
+        choices=StatusHorario.choices,
+        default=StatusHorario.DISPONIVEL
+    )
 
     def __str__(self):
         return f"{self.agenda.especialista} | {self.horario_inicio}/{self.horario_fim} - {self.status}"
 
 
-class Consulta(BaseModel):
+class Consulta(CommonModel):
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE, related_name='consultas')
     horario_gerado = models.OneToOneField(HorarioGerado, on_delete=models.CASCADE, related_name='consulta')
 
--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +76,16 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'agendamentos_db'),
+        'USER': os.environ.get('POSTGRES_USER', 'usuario_db'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'senha123'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': '5432',
     }
 }
 

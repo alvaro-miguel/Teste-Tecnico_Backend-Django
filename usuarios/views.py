3af6e239e61f5535847973a48a2a3f6fb167a478
alvaro-miguel <alvaromiguelrodrigues12@gmail.com>
2026-08-26 21:54:22 -0300

    refactor: Isolando geracao de horarios com signals e protegendo endpoints

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Especialista, Paciente
 from .serializers import EspecialistaSerializer, PacienteSerializer
+from agendamentos.permissions import IsInterno
 
 # Create your views here.
 
 class EspecialistaViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsInterno]
     queryset = Especialista.objects.all()
     serializer_class = EspecialistaSerializer
 
 class PacienteViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsInterno]
     queryset = Paciente.objects.all()
     serializer_class = PacienteSerializer
